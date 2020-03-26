23d538fb397a0cb0b8873e4182bcac727ad6828a
Majikujanisch <karottennase123@gmail.com>
2020-03-26 09:05:19 +0100

    data change

--- Amazon/src/Data.java
+++ Amazon/src/Data.java
@@ -1,16 +1,16 @@
 import models.Catalog;
 import models.User;
 import models.UserManager;
+import models.Item;
 
 import java.util.ArrayList;
 
 public interface Data {
-    void loadUsers(String filename, UserManager um);
-    void loadItems(String filename, Catalog catalog);
-    void saveUsers(String filename, ArrayList<User> users);
 
+    void saveUsers(ArrayList<User> users);
     void loadUsers(UserManager um);
     void loadItems(Catalog catalog);
+    void saveItems(ArrayList<Item> item);
 
     boolean checkIfUserDataAvailable();
 
